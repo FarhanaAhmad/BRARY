86844615bc2240e834f72854f0cb210c25b66c4b
FarhanaAhmad <fgm199703@gmail.com>
2020-06-04 23:42:59 +0500

    checking

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,7 +6,7 @@ const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
-const ITEMS_PER_PAGE = 2;
+const ITEMS_PER_PAGE = 6;
 
 exports.getProducts = (req, res, next) => {
   const page = +req.query.page || 1;
@@ -131,30 +131,6 @@ exports.postCartDeleteProduct = (req, res, next) => {
     .removeFromCart(prodId)
     .then(result => {
       res.redirect('/cart');
-    })
-    .catch(err => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
-    });
-};
-
-exports.getCheckout = (req, res, next) => {
-  req.user
-    .populate('cart.items.productId')
-    .execPopulate()
-    .then(user => {
-      const products = user.cart.items;
-      let total = 0;
-      products.forEach(p => {
-        total += p.quantity * p.productId.price;
-      });
-      res.render('shop/checkout', {
-        path: '/checkout',
-        pageTitle: 'Checkout',
-        products: products,
-        totalSum: total
-      });
     })
     .catch(err => {
       const error = new Error(err);
