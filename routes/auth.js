d22bf9a5fce9bb44367d5e99f3b2635a5b54b7b0
Hira1122 <hirasultan.uos@gmail.com>
2020-06-09 21:49:44 +0500

    change in router

--- routes/auth.js
+++ routes/auth.js
@@ -69,6 +69,7 @@ router.post('/logout', authController.postLogout);
 router.get('/reset', authController.getReset);
 
 router.post('/reset', authController.postReset);
+router.post('/reset', authController.postReset);
 
 router.get('/reset/:token', authController.getNewPassword);
 
